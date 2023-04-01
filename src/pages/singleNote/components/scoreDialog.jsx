bab0446ffed4796e9c7df1d499c219da81449fc6
Jonathan <jonathan@amitkohn.com>
2023-04-01 10:47:35 +0300

    corrected score

--- src/pages/singleNote/components/scoreDialog.jsx
+++ src/pages/singleNote/components/scoreDialog.jsx
@@ -4,7 +4,7 @@ import Dialog from "@mui/material/Dialog";
 const ScoreDialog = ({open, onClose, correct, time, asked, difficulty}) => {
     const correctRate = Math.round((correct*100) / asked);
     const correctTime = Math.round(time/correct);
-    const score = Math.round(3*correctRate*difficulty / correctTime);
+    const score = Math.round(correctRate*difficulty / correctTime);
     return(
         <Dialog onClose={onClose} open={open}>
             <h1>Correct Rate: {correctRate}%</h1>
